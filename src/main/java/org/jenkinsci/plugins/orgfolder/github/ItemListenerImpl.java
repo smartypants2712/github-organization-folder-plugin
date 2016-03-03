d74622eb18e287fe9eedb7719e344e17611993e7
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-03-03 10:25:42 -0800

    Trying a few different ways to hook in

--- src/main/java/org/jenkinsci/plugins/orgfolder/github/ItemListenerImpl.java
+++ src/main/java/org/jenkinsci/plugins/orgfolder/github/ItemListenerImpl.java
@@ -1,5 +1,6 @@
 package org.jenkinsci.plugins.orgfolder.github;
 
+import hudson.Extension;
 import hudson.model.Item;
 import hudson.model.listeners.ItemListener;
 import jenkins.branch.OrganizationFolder;
@@ -14,19 +15,25 @@ import java.util.logging.Logger;
 /**
  * @author Kohsuke Kawaguchi
  */
+@Extension
 public class ItemListenerImpl extends ItemListener {
     @Inject
     private MainLogic main;
 
     @Override
     public void onUpdated(Item item) {
+        super.onUpdated(item);
+    }
+
+    @Override
+    public void onCreated(Item item) {
         if (item instanceof OrganizationFolder) {
             OrganizationFolder of = (OrganizationFolder)item;
             if (of.getNavigators().size()>0) {
                 SCMNavigator n = of.getNavigators().get(0);
                 if (n instanceof GitHubSCMNavigator) {
                     try {
-                        main.apply(of, (GitHubSCMNavigator)n);
+                        main.setup(of);
                     } catch (IOException e) {
                         LOGGER.log(Level.WARNING, "Failed to apply GitHub Org Folder theme",e);
                     }
--- src/main/java/org/jenkinsci/plugins/orgfolder/github/MainLogic.java
+++ src/main/java/org/jenkinsci/plugins/orgfolder/github/MainLogic.java
@@ -1,23 +1,51 @@
 package org.jenkinsci.plugins.orgfolder.github;
 
-import com.cloudbees.plugins.credentials.CredentialsNameProvider;
 import com.cloudbees.plugins.credentials.common.StandardCredentials;
 import hudson.Extension;
+import hudson.model.Item;
 import jenkins.branch.OrganizationFolder;
+import jenkins.model.Jenkins;
 import jenkins.scm.api.SCMNavigator;
-import jenkins.util.io.FileBoolean;
 import org.jenkinsci.plugins.github_branch_source.Connector;
 import org.jenkinsci.plugins.github_branch_source.GitHubSCMNavigator;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 @Extension
 public class MainLogic {
+    /**
+     * Applies UI customizations to a newly created {@link OrganizationFolder}
+     * with a sole {@link GitHubSCMNavigator}
+     */
+    public void setup(OrganizationFolder of) throws IOException {
+        if (of.getProperties().get(GitHubOrgTheme.class)==null) {
+            of.addProperty(new GitHubOrgTheme());
+        }
+    }
+
+    public void maybeApply(Item item) {
+        if (item instanceof OrganizationFolder) {
+            OrganizationFolder of = (OrganizationFolder)item;
+            if (of.getNavigators().size()>0) {
+                SCMNavigator n = of.getNavigators().get(0);
+                if (n instanceof GitHubSCMNavigator) {
+                    try {
+                        apply(of, (GitHubSCMNavigator)n);
+                    } catch (IOException e) {
+                        LOGGER.log(Level.WARNING, "Failed to apply GitHub Org Folder theme",e);
+                    }
+                }
+            }
+        }
+    }
+
     /**
      * Applies UI customizations to a newly created {@link OrganizationFolder}
      * with a sole {@link GitHubSCMNavigator}
@@ -32,4 +60,10 @@ public class MainLogic {
         StandardCredentials credentials = Connector.lookupScanCredentials(of, n.getApiUri(), n.getScanCredentialsId());
         return Connector.connect(n.getApiUri(), credentials);
     }
+
+    public static MainLogic get() {
+        return Jenkins.getInstance().getInjector().getInstance(MainLogic.class);
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(MainLogic.class.getName());
 }
