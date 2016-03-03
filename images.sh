1b046cc31fd65d19fd3a327802ec2a10bfaf526a
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-03-03 11:05:04 -0800

    Adding more images

--- images.sh
+++ images.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd src/main/webapp/images
-for sz in 16 24 32 48 64;
+for n in logo repo;
 do
-  inkscape -z -e ${sz}x${sz}.png -w ${sz} -h ${sz} logo.svg
+  pushd $n
+    for sz in 16 24 32 48 64;
+    do
+      inkscape -z -e ${sz}x${sz}.png -w ${sz} -h ${sz} image.svg
+    done
+  popd
 done
