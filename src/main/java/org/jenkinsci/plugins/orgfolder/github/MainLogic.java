73a0b2c04f3634b50417005ea12baf32e185cf92
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-03-04 09:39:27 -0800

    Resurrected https://github.com/jenkinsci/github-branch-source-plugin/pull/13
    Code that auto-registers webhook

--- src/main/java/org/jenkinsci/plugins/orgfolder/github/MainLogic.java
+++ src/main/java/org/jenkinsci/plugins/orgfolder/github/MainLogic.java
@@ -18,17 +18,25 @@ import jenkins.branch.Branch;
 import jenkins.branch.OrganizationFolder;
 import jenkins.model.Jenkins;
 import jenkins.scm.api.SCMSourceOwner;
+import jenkins.util.io.FileBoolean;
 import org.jenkinsci.plugins.github_branch_source.Connector;
 import org.jenkinsci.plugins.github_branch_source.GitHubSCMNavigator;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
+import org.kohsuke.github.GHEvent;
+import org.kohsuke.github.GHHook;
+import org.kohsuke.github.GHOrganization;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static java.util.Arrays.*;
@@ -69,6 +77,25 @@ public class MainLogic {
                     of.deleteView(of.getView("All"));
                     of.setPrimaryView(lv);
                 }
+
+                FileBoolean orghook = new FileBoolean(new File(of.getRootDir(),"GitHubOrgHook."+scm.getRepoOwner()));
+                if (orghook.isOff()) {
+                    try {
+                        GHOrganization org = hub.getOrganization(scm.getRepoOwner());
+                        String url = Jenkins.getActiveInstance().getRootUrl() + "github-webhook/";
+                        if (!existsHook(org, url)) {
+                            org.createWebHook(new URL(url), Arrays.asList(GHEvent.REPOSITORY,GHEvent.PUSH));
+                            LOGGER.log(Level.INFO, "A webhook was registered for the organization {0}", org.getHtmlUrl());
+                        }
+                        // keep trying until the hook gets successfully installed
+                        // if the user doesn't have the proper permission, this will cause
+                        // a repeated failure, but this code doesn't execute too often.
+                        orghook.on();
+                    } catch (IOException e) {
+                        LOGGER.log(Level.WARNING, "Failed to register GitHub Org hook to "+u.getHtmlUrl(), e);
+                    }
+                }
+
 
                 bc.commit();
             } finally {
@@ -146,6 +173,18 @@ public class MainLogic {
         return Connector.connect(n.getApiUri(), credentials);
     }
 
+    /**
+     * Verify if exists a webhook by its URL.
+     */
+    private boolean existsHook(GHOrganization org, String url) throws IOException {
+        for (GHHook hook : org.getHooks()) {
+            if (hook.getConfig().get("url").equals(url)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static MainLogic get() {
         return Jenkins.getActiveInstance().getInjector().getInstance(MainLogic.class);
     }
@@ -160,4 +199,6 @@ public class MainLogic {
             return new HashSet<>();
         }
     };
+
+    private static final Logger LOGGER = Logger.getLogger(MainLogic.class.getName());
 }
