405c6cb87623161b0b06b3c9655259f2ff1c41c0
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-03-03 17:50:32 -0800

    More domain specific term than "Folder computation"

--- src/main/java/org/jenkinsci/plugins/orgfolder/github/AlternativeUiTextProviderImpl.java
+++ src/main/java/org/jenkinsci/plugins/orgfolder/github/AlternativeUiTextProviderImpl.java
@@ -1,5 +1,7 @@
 package org.jenkinsci.plugins.orgfolder.github;
 
+import com.cloudbees.hudson.plugins.folder.computed.ComputedFolder;
+import com.cloudbees.hudson.plugins.folder.computed.FolderComputation;
 import hudson.Extension;
 import hudson.model.AbstractItem;
 import hudson.util.AlternativeUiTextProvider;
@@ -18,6 +20,12 @@ public class AlternativeUiTextProviderImpl extends AlternativeUiTextProvider {
             }
             if (Sniffer.matchBranch(i)!=null) {
                 return "Branch";
+            }
+        }
+        if (text== FolderComputation.DISPLAY_NAME) {
+            ComputedFolder<?> f = FolderComputation.DISPLAY_NAME.cast(FolderComputation.class).getParent();
+            if (Sniffer.matchOrg(f)!=null) {
+                return "Re-scan GitHub Org";
             }
         }
         return null;
